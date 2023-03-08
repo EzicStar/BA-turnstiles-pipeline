8b45d780cf08ea4fcf7f8b2112cd78dd9e46a396
EzicStar <juanpabloricapito@gmail.com>
2023-03-07 23:27:53 -0300

    Terraform
    Files needed for creating a gcs bucket an d a big query dataset with terraform.

---
+++ Terraform/variables.tf
@@ -0,0 +1,29 @@
+locals {
+  data_lake_bucket = "ba-turnstiles"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "us-central1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "turnstiles_data"
+}
+
+variable "credentials" {
+  description = "Credentials file location in your computer"
+  type = string
+}
